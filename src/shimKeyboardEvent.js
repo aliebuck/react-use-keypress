0c1ad39cca703489620007c711f7e097b48cb8d6
Alie Buck <830470+aliebuck@users.noreply.github.com>
2025-12-07 16:18:24 +1300

    refactor: simplify shimKeyboardEvent

--- src/shimKeyboardEvent.js
+++ src/shimKeyboardEvent.js
@@ -1,35 +1,32 @@
 // Fixing inconsistencies from older browsers
 // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
-const aliases = new Map([
-  ["Win", "Meta"],
-  ["Scroll", "ScrollLock"],
-  ["Spacebar", " "],
-  ["Down", "ArrowDown"],
-  ["Left", "ArrowLeft"],
-  ["Right", "ArrowRight"],
-  ["Up", "ArrowUp"],
-  ["Del", "Delete"],
-  ["Crsel", "CrSel"],
-  ["Exsel", "ExSel"],
-  ["Apps", "ContextMenu"],
-  ["Esc", "Escape"],
-  ["Decimal", "."],
-  ["Multiply", "*"],
-  ["Add", "+"],
-  ["Subtract", "-"],
-  ["Divide", "/"],
-]);
+const aliases = {
+  Win: "Meta",
+  Scroll: "ScrollLock",
+  Spacebar: " ",
+  Down: "ArrowDown",
+  Left: "ArrowLeft",
+  Right: "ArrowRight",
+  Up: "ArrowUp",
+  Del: "Delete",
+  Crsel: "CrSel",
+  Exsel: "ExSel",
+  Apps: "ContextMenu",
+  Esc: "Escape",
+  Decimal: ".",
+  Multiply: "*",
+  Add: "+",
+  Subtract: "-",
+  Divide: "/",
+};
 
 const shimKeyboardEvent = (event) => {
-  if (aliases.has(event.key)) {
-    const key = aliases.get(event.key);
-
+  const originalKey = event.key;
+  if (Object.hasOwn(aliases, originalKey)) {
     Object.defineProperty(event, "key", {
       configurable: true,
       enumerable: true,
-      get() {
-        return key;
-      },
+      get: () => aliases[originalKey],
     });
   }
 };
