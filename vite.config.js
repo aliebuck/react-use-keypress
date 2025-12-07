afdde3321220f06a78257173f23a3116b72b5672
Alie Buck <830470+aliebuck@users.noreply.github.com>
2025-12-07 15:44:11 +1300

    chore: replace babel and rollup with vite

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from 'node:path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'tiny-invariant'],
+    },
+    sourcemap: true,
+  },
+  plugins: [react()],
+});
