e452b4a01b19e00ef19ef9b07e6b968a87909466
Alie Buck <830470+aliebuck@users.noreply.github.com>
2025-12-08 18:52:57 +1300

    chore: add jsdoc to useKeypress

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { useEffect, useRef } from "react";
 import shimKeyboardEvent from "./shimKeyboardEvent";
 
+/**
+ * Registers a keydown listener that triggers a handler when one or more
+ * specified keys are pressed. Accepts either a single key or an array of keys.
+ * @param {string|string[]} keys - Key or list of keys to listen for.
+ * @param {(event: KeyboardEvent) => void} [handler] - Function called when a matching key is pressed.
+ */
 const useKeypress = (keys, handler) => {
   const eventListenerRef = useRef();
 
